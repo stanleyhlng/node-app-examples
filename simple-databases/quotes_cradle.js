f6b7c55f72919e216a36a4c4a2161c8e0593f1d0
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-08-28 22:37:16 +0800

    updated quotes_cradle.js

--- simple-databases/quotes_cradle.js
+++ simple-databases/quotes_cradle.js
@@ -12,6 +12,40 @@ function errorHandler(err) {
 	}
 }
 
+function outputQuotes(err) {
+	errorHandler(err);
+	if (params.author) {
+		db.view('quotes/byAuthor', {
+			key: params.author
+			},
+			function (err, rowsArray) {
+				if (err && err.error === "not_found") {
+					createQuotesView();
+					return;
+				}
+				errorHandler(err);
+				rowsArray.forEach(function(doc) {
+					console.log("%s: %s \n", doc.author, doc.quote);
+					return;
+				});
+			}
+		);
+	}
+}
+
+function createQuotesView(err) {
+	errorHandler(err);
+	db.save('_design/quotes', {
+		views: {
+			byAuthor: {
+				map: 'function (doc) { emit(doc.author, doc) }'
+			}
+		}
+	},
+	outputQuotes
+	);
+}
+
 function checkAndSave(err) {
 	errorHandler(err);
 	if (params.author && params.quote) {
@@ -19,8 +53,11 @@ function checkAndSave(err) {
 			author: params.author,
 			quote: params.quote
 		},
-		errorHandler);
+		outputQuotes
+		);
+		return;
 	}
+	outputQuotes();
 }
 
 db.exists(function(err, exists) {
