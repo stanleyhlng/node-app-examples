7db2ea2932ef5d3d84cdd549b5531e6641644f8d
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-08-29 14:55:22 +0800

    added client-web-socket and client-socket-io

---
+++ simple-web-socket/server-socket-io.js
@@ -0,0 +1,22 @@
+var http = require('http'),
+    client = require('fs').readFileSync('client-socket-io.html');
+
+var plainHttpServer = http.createServer(function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end(client);
+})
+.listen(8080);
+
+var io = require('socket.io').listen(plainHttpServer);
+
+io.set('origins', ['localhost:8080', '127.0.0.1:8080']);
+
+io.sockets.on('connection', function(socket) {
+    socket.on('message', function(msg) {
+        if (msg == "Hello") {
+            socket.send('socket.io!');
+        }
+    });
+});
